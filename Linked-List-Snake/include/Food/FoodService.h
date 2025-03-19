78319a832b35ea2150ba0851cc02cde5cd57efac
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-03-19 08:35:56 +0530

    food spawning in intervals

--- Linked-List-Snake/include/Food/FoodService.h
+++ Linked-List-Snake/include/Food/FoodService.h
@@ -1,41 +1,61 @@
 #pragma once
 #include <SFML/System/Vector2.hpp>
-#include "Food/FoodType.h" // I added due to getting error in the food type in .cpp file 
 #include <random>
-
 #include <vector>
 
 namespace Food
 {
-	//enum class FoodType; // earlier it was the forward declaration 
+	enum class FoodType;
 	class FoodItem;
+
+	enum FoodSpawningStatus
+	{
+		ACTIVE,
+		IN_ACTIVE,
+	};
 
 	class FoodService
 	{
 	private:
+		const float spawn_duration = 4.f;
+
+		float elapsed_duration;
+
+		FoodSpawningStatus current_spawning_status;
 		FoodItem* current_food_item;
 
 		float cell_width;
 		float cell_height;
 
+		// To generate random values.
+		std::default_random_engine random_engine;
+
+		// To give random seed to generator.
+		std::random_device random_device;
+
 		FoodItem* createFood(sf::Vector2i position, FoodType type);
 		void spawnFood();
+
+		sf::Vector2i getValidSpawnPosition();
 		sf::Vector2i getRandomPosition();
-		void destroyFood();
-		std::default_random_engine random_engine;
-		std::random_device random_device;
+		FoodType getRandomFoodType();
+
 		bool isValidPosition(std::vector<sf::Vector2i> position_data, sf::Vector2i food_position);
-		
+
+		void destroyFood();
+		void updateElapsedDuration();
+		void handleFoodSpawning();
+		void reset();
+
 	public:
 		FoodService();
 		~FoodService();
-		
+
 		void initialize();
 		void update();
 		void render();
-		sf::Vector2i getValidSpawnPosition();
-		FoodType getRandomFoodType();
-		
+
 		void startFoodSpawning();
+		void stopFoodSpawning();
 	};
 }
